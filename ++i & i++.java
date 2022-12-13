c7ecb7be412dd1264c30cb1f423c066a6de47096
rafaelmaribojoc <68845906+rafaelmaribojoc@users.noreply.github.com>
2022-12-13 16:49:21 +0800

    Blah
    Blahblah

---
+++ ++i & i++.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 0; i<5;) {
+		      
+        System.out.printf("%d ", ++i); //increment then return value
+        System.out.printf("%d ", i++); //return value before increment
+		
+		}
+	}
+}
